30fb2dd3327ef3886b25b8c7a716a5eb657ef2dd
Edmund <edmundcjj@gmail.com>
2018-04-03 14:13:37 +0800

    Second commit - added sql files

---
+++ seed.sql
@@ -0,0 +1,8 @@
+
+-- populate table with users
+
+-- populate table with itineraries
+
+-- populate table with days in a itinerary
+
+-- populate table with cards in a day
---
+++ tables.sql
@@ -0,0 +1,30 @@
+-- create users table
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  name varchar(255),
+  email varchar(255) not null unique,   -- make email field non-case sensitive and unique
+  password varchar(255) not null
+);
+
+-- create itineraries table that contains itinerary belonging to a user
+CREATE TABLE IF NOT EXISTS itineraries (
+  id SERIAL PRIMARY KEY,
+  u_id integer not null
+);
+
+-- create itinerary_days table that contains a reference of days in each itinerary
+CREATE TABLE IF NOT EXISTS itinerary_days (
+  id SERIAL PRIMARY KEY,
+  i_id integer not null
+);
+
+-- create day_card table that contains itinerary data of a single day in a single itinerary
+CREATE TABLE IF NOT EXISTS day_card (
+  id SERIAL PRIMARY KEY,
+  d_id integer not null,
+  start_at time,
+  end_at time,
+  image varchar(255),
+  location text not null,
+  description text
+)
