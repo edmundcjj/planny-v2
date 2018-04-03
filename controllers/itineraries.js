b23791e57ef849edb72facc03811ba50c0f47c34
Edmund <edmundcjj@gmail.com>
2018-04-03 14:08:46 +0800

    Second commit - added controller files

---
+++ controllers/itineraries.js
@@ -0,0 +1,119 @@
+/**
+ * Itineraries controller functions.
+ *
+ * Each itinerary-related route in `routes.js` will call
+ * one controller function here.
+ *
+ * Export all functions as a module using `module.exports`,
+ * to be imported (using `require(...)`) in `routes.js`.
+ */
+
+/**
+ * ===========================================
+ * Controller logic
+ * ===========================================
+ */
+// // Logic to retrieve data of a specific pokemon and display on the page
+// const get = (pokemonModel) => {
+//   return (request, response) => {
+//     // use pokemon model method `get` to retrieve pokemon data
+//     pokemonModel.pokemon.get(request.params.id, (error, queryResult) => {
+//       // queryResult contains pokemon data returned from the pokemon model
+//       if (error) {
+//         console.error('error getting pokemon:', error);
+//         response.sendStatus(500);
+//       } else {
+//         // render pokemon.handlebars in the pokemon folder
+//         response.render('pokemon/pokemon', {
+//           pokemon: queryResult.rows[0]
+//         });
+//       }
+//     });
+//   };
+// };
+//
+// // Retrieve data of a specific pokemon and render into the fields of the update form
+// const updateForm = (pokemonModel) => {
+//   return (request, response) => {
+//     // TODO: Add logic here
+//     // use pokemon model method `get` to retrieve pokemon data
+//     pokemonModel.pokemon.get(request.params.id, (error, queryResult) => {
+//       console.log(queryResult.rows[0]);
+//       // queryResult contains pokemon data returned from the pokemon model
+//       if (error) {
+//         console.error('error getting pokemon:', error);
+//         response.sendStatus(500);
+//       } else {
+//         // render pokemon.handlebars in the pokemon folder
+//         response.render('pokemon/edit', {
+//           pokemon: queryResult.rows[0]
+//         });
+//       }
+//     });
+//   };
+// };
+//
+// // Update data of existing pokemon from the update form
+// const update = (pokemonModel) => {
+//   return (request, response) => {
+//     // TODO: Add logic here
+//     // use pokemon model method 'update' to update a specific stats of a pokemon
+//     pokemonModel.pokemon.update(request.body, (error, queryResult) => {
+//       console.log("Controller - updating in progress...");
+//       let poke_id = request.params.id;
+//
+//       if (error) {
+//         console.error('error updating pokemon:', error);
+//         response.sendStatus(500);
+//       } else {
+//         // redirect back to display updated stats of a specific pokemon
+//         console.log("Controller - update finished...");
+//         response.redirect('/pokemons/' + poke_id);
+//       }
+//     });
+//   };
+// };
+//
+// // Render the create form to create new pokemon
+// const createForm = (request, response) => {
+//   response.render('pokemon/new');
+// };
+//
+// // Logic to create the new pokemon
+// const create = (pokemonModel) => {
+//   return (request, response) => {
+//     // use pokemon model method `create` to create new pokemon entry in db
+//     pokemonModel.pokemon.create(request.body, (error, queryResult) => {
+//       // queryResult of creation is not useful to us, so we ignore it
+//       // (console log it to see for yourself)
+//       // (you can choose to omit it completely from the function parameters)
+//
+//       if (error) {
+//         console.error('error getting pokemon:', error);
+//         response.sendStatus(500);
+//       }
+//
+//       if (queryResult.rowCount >= 1) {
+//         console.log('Pokemon created successfully');
+//       } else {
+//         console.log('Pokemon could not be created');
+//       }
+//
+//       // redirect to home page after creation
+//       response.redirect('/');
+//     });
+//   };
+// };
+
+/**
+ * ===========================================
+ * Export controller functions as a module
+ * ===========================================
+ */
+// module.exports = {
+//   get,
+//   updateForm,
+//   update,
+//   createForm,
+//   create
+// };
---
+++ controllers/user.js
@@ -0,0 +1,100 @@
+/**
+ * User controller functions.
+ *
+ * Each user-related route in `routes.js` will call
+ * one controller function here.
+ *
+ * Export all functions as a module using `module.exports`,
+ * to be imported (using `require(...)`) in `routes.js`.
+ */
+
+
+/**
+ * ===========================================
+ * Controller logic
+ * ===========================================
+ */
+// // Render the form to create new users
+// const newForm = (request, response) => {
+//   response.render('user/new');
+// };
+//
+// // Logic to create new user
+// const create = (userModel) => {
+//   return (request, response) => {
+//     // use user model method `create` to create new user entry in db
+//     userModel.user.create(request.body, (error, queryResult) => {
+//       // queryResult of creation is not useful to us, so we ignore it
+//       // (console log it to see for yourself)
+//       // (you can choose to omit it completely from the function parameters)
+//
+//       if (error) {
+//         console.error('error getting pokemon:', error);
+//         response.sendStatus(500);
+//       }
+//
+//       if (queryResult.rowCount >= 1) {
+//         console.log('User created successfully');
+//
+//         // drop cookies to indicate user's logged in status and username
+//         response.cookie('loggedIn', true);
+//         response.cookie('username', request.body.name);
+//       } else {
+//         console.log('User could not be created');
+//       }
+//
+//       // redirect to home page after creation
+//       response.redirect('/');
+//     });
+//   };
+// };
+//
+// // Logic to clear cookies when user logouts and redirect back to home
+// const logout = (request, response) => {
+//   response.clearCookie('loggedIn');
+//   response.clearCookie('username');
+//   response.redirect(301, '/');
+// };
+//
+// // Logic to render the form for user login
+// const loginForm = (request, response) => {
+//   // Redirect user back to the default page if user is already logged in
+//   if (request.cookies['loggedIn'] == 'true') {
+//     response.redirect('/');
+//   } else {
+//     response.render('user/login');
+//   }
+// };
+//
+// // Logic to verify user login details
+// const login = (userModel) => {
+//   return (request, response) => {
+//     userModel.user.login(request.body, (error, queryResult) => {
+//       // User verified, redirect back to default page
+//       if (queryResult) {
+//         let user_name = request.body.name;
+//         response.cookie('loggedIn', true);
+//         response.cookie('username', user_name);
+//         response.redirect('/');
+//       }
+//       // User not verified, redirect back to the login page
+//       else {
+//         console.log("Unsuccessful Login...");
+//         response.redirect('/users/login');
+//       }
+//     });
+//   };
+// };
+
+/**
+ * ===========================================
+ * Export controller functions as a module
+ * ===========================================
+ */
+// module.exports = {
+//   newForm,
+//   create,
+//   logout,
+//   loginForm,
+//   login
+// };
