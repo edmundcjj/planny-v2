de2b5c358022d377a096050685609885b9d1354f
Edmund <edmundcjj@gmail.com>
2018-04-06 13:45:39 +0800

    Commit 11 - Added custom javascript file for the text animation

---
+++ public/js/home.js
@@ -0,0 +1,24 @@
+
+console.log("Inside home.js file to animate the letters...");
+
+// Wrap every letter in a span
+$('.ml6 .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: function(el, i) {
+      return 50 * i;
+    }
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
